dc4cca24647cf898a469ac58ee60a36306d36c74
JUNIOR0005 <144936537+JUNIOR0005@users.noreply.github.com>
2023-09-18 09:22:51 -0600

    Add files via upload

---
+++ FINAL PROJECT -- PERSONAL WEB/script.js
@@ -0,0 +1,31 @@
+javascript
+Copy code
+// Smooth scrolling for navigation links
+document.querySelectorAll('nav a').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        const targetId = this.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        if (targetSection) {
+            window.scrollTo({
+                top: targetSection.offsetTop - 50,
+                behavior: 'smooth'
+            });
+        }
+    });
+});
+
+// Form submission handling
+const contactForm = document.getElementById('contact-form');
+contactForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    const message = document.getElementById('message').value;
+
+    // You can add code here to send the form data to your server or handle it as needed
+
+    // For demonstration purposes, show a confirmation message
+    alert(`Thank you, ${name}! Your message has been sent.`);
+    contactForm.reset();
+});
