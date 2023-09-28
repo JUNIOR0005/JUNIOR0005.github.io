81ace5889bb8826106e87e7b87a16c902e145217
Angel.B <144936537+JUNIOR0005@users.noreply.github.com>
2023-09-28 17:56:17 -0600

    Add files via upload

---
+++ sumar/calculadora.js
@@ -0,0 +1,9 @@
+function suma(){
+    var numero1 = parseFloat(document.getElementById('numero1').value);
+    var numero2 = parseFloat(document.getElementById('numero2').value);
+    var resultado = numero1+numero2;
+var
+
+    document.getElementById('resultado').value=resultado;
+    
+}
